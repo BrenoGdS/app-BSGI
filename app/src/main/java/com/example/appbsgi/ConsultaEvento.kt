9d1882100d88fd35001f06fed04792a814004572
MauricioAlves-Git <61766831+MauricioAlves-Git@users.noreply.github.com>
2021-11-06 16:09:21 -0300

    EventoConsulta
    CommitMauricio

---
+++ app/src/main/java/com/example/appbsgi/ConsultaEvento.kt
@@ -0,0 +1,11 @@
+package com.example.appbsgi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultaEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consulta_evento)
+    }
+}
