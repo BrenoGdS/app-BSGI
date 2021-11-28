0e7d62afa720a95acee3d9609aa5d84f77be851d
BrenoGdS <breno.eletronica@gmail.com>
2021-11-28 20:15:33 -0300

    consulta eventos
    consulta eventos

--- app/src/main/java/com/example/appbsgi/ConsultaEvento.kt
+++ app/src/main/java/com/example/appbsgi/ConsultaEvento.kt
@@ -8,6 +8,7 @@ import com.android.volley.Response
 import com.android.volley.toolbox.Volley
 import model.Evento
 import org.json.JSONArray
+import java.time.LocalDateTime
 import com.android.volley.toolbox.StringRequest as StringRequest1
 
 class ConsultaEvento : AppCompatActivity() {
@@ -15,9 +16,10 @@ class ConsultaEvento : AppCompatActivity() {
     lateinit var editOrganizacao: EditText
     lateinit var spinnerTipoEvento: Spinner
     lateinit var spinnerCidade: Spinner
-    lateinit var dateNiver: DatePicker
+    lateinit var dataEvento: DatePicker
     lateinit var buttonPesquisar: Button
     lateinit var listViewEventos: ListView
+    lateinit var arrayEventos: ArrayList<Evento>
 
 
 
@@ -27,30 +29,51 @@ class ConsultaEvento : AppCompatActivity() {
         setContentView(R.layout.activity_consulta_evento)
         arrayEventos = ArrayList<Evento>()
 
+        editTituloEvento = findViewById(R.id.editTituloEvento)
+        editOrganizacao = findViewById(R.id.editOrganizacao)
+        spinnerTipoEvento = findViewById(R.id.spinnerTipoEvento)
+        spinnerCidade = findViewById(R.id.spinnerCidade)
+        dataEvento = findViewById(R.id.dataEvento)
+        buttonPesquisar = findViewById(R.id.buttonPesquisar)
         listViewEventos = findViewById(R.id.listViewEventos)
-        listViewEventos.setOnItemClickListener{ parent, view, position, id ->
-            evento = Evento()
-            editTituloEvento = findViewById(R.id.editTituloEvento)
 
+        buttonPesquisar.setOnClickListener() {
+            carregarEventosAPI()
+        }
 
-                arrayTimes[position].idevento,
-                arrayTimes[position].organizacao,
-                arrayTimes[position].idtipoevento,
-                arrayTimes[position].titulo,
-                arrayTimes[position].dataevento,
-                arrayTimes[position].cepevento,
-                arrayTimes[position].idcidadeevento),
-                arrayTimes[position].logradouroevento,
-                arrayTimes[position].numevento,
-                arrayTimes[position].complementoevento,
-                arrayTimes[position].bairroevento)
-
-
-            )
+        listViewEventos.setOnItemClickListener { parent, view, position, id ->
+            var evento = Evento(
+                arrayEventos[position].idevento,
+                arrayEventos[position].idtipoevento,
+                arrayEventos[position].titulo,
+                arrayEventos[position].dataevento,
+                arrayEventos[position].cepevento,
+                arrayEventos[position].idcidadeevento,
+                arrayEventos[position].logradouroevento,
+                arrayEventos[position].numevento,
+                arrayEventos[position].complementoevento,
+                arrayEventos[position].bairroevento,
+                arrayEventos[position].desctipoEvento,
+                arrayEventos[position].nomeOrg,
+                arrayEventos[position].descCidade)
+        }
     }
 
     fun carregarEventosAPI() {
-        val url = "https://apiaulamauricio.000webhostapp.com/apiPI/getAllEventos.php"
+
+        var evento = Evento(
+            editTituloEvento.text.toString(),
+            editOrganizacao.text.toString(),
+            spinnerTipoEvento.selectedItem as String,
+            spinnerCidade.selectedItem as String,
+            dataEvento.year.toString() + '-' + dataEvento.month.toString() + '-' + dataEvento.dayOfMonth.toString()
+        )
+        val url = "https://apimobileaularodrigo.000webhostapp.com/apiPI/getAllEventos.php?" +
+                "HTTP_TITULO=${evento.titulo}" +
+                "&HTTP_NOMEORG=${evento.nomeOrg}" +
+                "&HTTP_DESCTIPOEVENTO=${evento.desctipoEvento}" +
+                "&HTTP_DESCCIDADE=${evento.descCidade}" +
+                "&HTTP_DATA=${evento.dataevento}"
         val stringRequest = StringRequest1(
             Request.Method.GET,
             url,
@@ -59,20 +82,20 @@ class ConsultaEvento : AppCompatActivity() {
                 for (i in 0..jsonArray.length() - 1) {
                     var jsonObject = jsonArray.getJSONObject(i)
 
-                    evento = Evento(
-                        jsonObject.getInt("idevento"),
-                        jsonObject.getString("organizacao"),
+                    var evento = Evento(jsonObject.getInt("idevento"),
+                        jsonObject.getInt("idtipoevento"),
                         jsonObject.getString("titulo"),
                         jsonObject.getString("dataevento"),
-                        jsonObject.getString("cepevento"),
-                        jsonObject.getString("cidadeevento"),
-                        jsonObject.getString("dataevento"),
-                        jsonObject.getString("logradouroevento "),
-                        jsonObject.getString("numevento "),
+                        jsonObject.getInt("cepevento"),
+                        jsonObject.getInt("idcidadeevento"),
+                        jsonObject.getString("logradouroevento"),
+                        jsonObject.getInt("numevento"),
                         jsonObject.getString("complementoevento"),
-                        jsonObject.getString("bairroevento  "),
-
-                        )
+                        jsonObject.getString("bairroevento"),
+                        jsonObject.getString("desctipoEvento"),
+                        jsonObject.getString("nomeOrg"),
+                        jsonObject.getString("descCidade")
+                    )
                     arrayEventos.add(evento);
                 }
 
--- app/src/main/java/model/Evento.kt
+++ app/src/main/java/model/Evento.kt
@@ -3,7 +3,6 @@ package model
 
 class Evento {
     var idevento: Int = 0
-    var organizacao: Int = 0
     var idtipoevento: Int = 0
     lateinit var titulo: String
     lateinit var dataevento: String
@@ -13,9 +12,50 @@ class Evento {
     var numevento: Int = 0
     lateinit var complementoevento: String
     lateinit var bairroevento: String
+    lateinit var desctipoEvento: String
+    lateinit var nomeOrg: String
+    lateinit var descCidade: String
+
+    constructor(
+        idevento: Int,
+        idtipoevento: Int,
+        titulo: String,
+        dataevento: String,
+        cepevento: Int,
+        idcidadeevento: Int,
+        logradouroevento: String,
+        numevento: Int,
+        complementoevento: String,
+        bairroevento: String,
+        desctipoEvento: String,
+        nomeOrg: String,
+        descCidade: String
+    ) {
+        this.idevento = idevento
+        this.idtipoevento = idtipoevento
+        this.titulo = titulo
+        this.dataevento = dataevento
+        this.cepevento = cepevento
+        this.idcidadeevento = idcidadeevento
+        this.logradouroevento = logradouroevento
+        this.numevento = numevento
+        this.complementoevento = complementoevento
+        this.bairroevento = bairroevento
+        this.desctipoEvento = desctipoEvento
+        this.nomeOrg = nomeOrg
+        this.descCidade = descCidade
+    }
+
+    constructor(titulo: String, nomeOrg: String, desctipoEvento: String, descCidade: String, dataevento: String) {
+        this.titulo = titulo
+        this.nomeOrg = nomeOrg
+        this.desctipoEvento = desctipoEvento
+        this.descCidade = descCidade
+        this.dataevento = dataevento
+    }
 
     override fun toString(): String {
-        return "Evento(idevento=$idevento, organizacao=$organizacao, idtipoevento=$idtipoevento, titulo='$titulo', dataevento='$dataevento', cepevento=$cepevento, idcidadeevento=$idcidadeevento, logradouroevento='$logradouroevento', numevento=$numevento, complementoevento='$complementoevento', bairroevento='$bairroevento')"
+        return "Evento(idevento=$idevento,  idtipoevento=$idtipoevento, titulo='$titulo', dataevento='$dataevento', cepevento=$cepevento, idcidadeevento=$idcidadeevento, logradouroevento='$logradouroevento', numevento=$numevento, complementoevento='$complementoevento', bairroevento='$bairroevento', desctipoEvento='$desctipoEvento', nomeOrg='$nomeOrg', descCidade='$descCidade')"
     }
 
 
